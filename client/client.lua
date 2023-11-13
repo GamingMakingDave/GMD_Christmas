a0cd9c8b14665758cd8a5768d316396149d76329
GMD <87772503+GamingMakingDave@users.noreply.github.com>
2023-11-14 00:59:52 +0100

    add config weather xmas + deers

--- client/client.lua
+++ client/client.lua
@@ -5,6 +5,7 @@ SantaClausSpawned = false
 Musicplay = false
 PlayerInSearch = false
 PlayerSearching = {}
+local PedCoords = {}
 
 AddEventHandler('onResourceStart', function(resource)
     if GetCurrentResourceName() == resource then 
@@ -14,7 +15,7 @@ AddEventHandler('onResourceStart', function(resource)
 end)
 
 CreateThread(function()
-    while true do
+    while Config.UseXmasWeather do
 		SetWeatherTypePersist("XMAS")
         SetWeatherTypeNowPersist("XMAS")
         SetWeatherTypeNow("XMAS")
@@ -99,7 +100,8 @@ CreateThread(function()
                         showsubtitle("Rehntiere siehst ich komme dann...", 1000)
                         Wait(1500)
                         TriggerServerEvent('GMD_Christmas:giveXmasSearchItem')
-                        TriggerServerEvent('GMD_Christmas:SpawnPeds')
+                        -- TriggerServerEvent('GMD_Christmas:SpawnPeds')
+
                     end
                 end
             end
@@ -110,25 +112,42 @@ CreateThread(function()
     end
 end)
 
--- CreateThread(function()
---     while true do
---         Wait(0)
---         local dist = #(GetEntityCoords(PlayerPedId()) - Config.SantaClausPosition)
---         if dist <= 2.0 and not LegalPressed then
---             ESX.ShowHelpNotification((Config.Language[Config.Local]['santa_helptext']))
---             if IsControlJustPressed(0, 38) then
---                 Wait(500)
---                 showsubtitle("Mir sind meine Rentiere entlaufen und mit euren geschenken abgehauen!!", 1000)
---                 Wait(1500)
---                 showsubtitle("Kannst du sie suchen helfen?", 1000)
---                 Wait(1500)
---                 showsubtitle("Ansonsten war es das mit dem Weihnachtsfest und ihr müsst ohne Geschenke auskommen", 1000)
---             else
---                 Wait(2000)
---             end
---         end
---     end
--- end)
+function SpawnSearchDears()
+    for _, v in ipairs(Config.DeerZones) do
+        local shuffled = {}
+
+        for i = 1, v.DeerCount do
+            local randIndex = math.random(1, #v.Coords)
+            table.insert(shuffled, v.Coords[randIndex])
+            table.remove(v.Coords, randIndex)
+        end
+
+        for i, Coords in ipairs(shuffled) do
+            RequestModel(GetHashKey(v.DeerPedModel))
+            while not HasModelLoaded(v.DeerPedModel) do
+                Wait(15)
+            end
+
+            local PedSpawn = CreatePed(4, GetHashKey(v.DeerPedModel), Coords.x, Coords.y, Coords.z, Coords.h, false, true)
+            SetPedArmour(PedSpawn, 200)
+            SetEntityMaxHealth(PedSpawn, 500)
+            SetEntityHealth(PedSpawn, 500)
+            TaskWanderInArea(PedSpawn, Coords.x, Coords.y, Coords.z, v.DeerZonesRadius, 1, 5.0)
+
+            local blip = AddBlipForEntity(PedSpawn)
+            SetBlipSprite(blip, 141)
+            SetBlipColour(blip, 1)
+            SetBlipAsShortRange(blip, true)
+
+            local blipLabel = "Santas Spur"
+            AddTextEntry("BLIP_NAME", blipLabel)
+            BeginTextCommandSetBlipName("BLIP_NAME")
+            AddTextComponentSubstringPlayerName(blipLabel)
+            EndTextCommandSetBlipName(blip)
+        end
+    end
+end
+
 
 function showsubtitle(text, time)
     ClearPrints()
@@ -139,6 +158,10 @@ end
 
 RegisterCommand("stopmusic", function(source, args, rawCommand)
     xSound:Destroy("Xmas")
+end, false)
+
+RegisterCommand("testDeer", function(source, args, rawCommand)
+    SpawnSearchDears()
 end, false)
 
 RegisterCommand("playmusic", function(source, args, rawCommand)
