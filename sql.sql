74e4f9ab915a57802864ec44416e5fadc60f8fde
GMD <87772503+GamingMakingDave@users.noreply.github.com>
2023-11-14 22:30:30 +0100

    add mission finished sql

--- sql.sql
+++ sql.sql
@@ -1,2 +1,12 @@
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
-('santas_present', 'Santas Present', 1, 0, 1);
+('christmas_stocking', 'Xmas Stocking', 1, 0, 1);
+
+CREATE TABLE `xmas` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `identifier` VARCHAR(60) NOT NULL,
+    `hasFinishedDeerGame` TINYINT(1) DEFAULT 0
+) ENGINE=InnoDB;
+
+ALTER TABLE `xmas`
+ADD COLUMN `id` INT AUTO_INCREMENT PRIMARY KEY FIRST;
+
