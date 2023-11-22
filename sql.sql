7345935b22bd7a250cd63529be17ad19024fb756
GMD <87772503+GamingMakingDave@users.noreply.github.com>
2023-11-22 01:52:45 +0100

    update sql

--- sql.sql
+++ sql.sql
@@ -3,3 +3,13 @@ CREATE TABLE `xmas` (
     `identifier` VARCHAR(60) NOT NULL,
     `hasFinishedDeerGame` TINYINT(1) DEFAULT 0
 ) 
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+('santas_sugar_stick', 'Santas Sugar Stick', 0, 0, 1),
+
+('gold_ticket', 'Santas Gold Ticket', 0, 0, 1),
+
+('sugar_cone', 'Sugar Cone', 0, 0, 1),
+('santas_coffee', 'Santas Smoke Coffee', 0, 0, 1),
+('santas_wine', 'Santas Smoke Wine', 0, 0, 1),
+('gingerbread', 'Police Jammer', 0, 0, 1);
