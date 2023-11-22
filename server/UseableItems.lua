63d8c5227ce566916be35defe7bd5a0e23eee350
GMD <87772503+GamingMakingDave@users.noreply.github.com>
2023-11-22 01:52:28 +0100

    add useableitems

---
+++ server/UseableItems.lua
@@ -0,0 +1,21 @@
+ESX = exports['es_extended']:getSharedObject()
+
+ESX.RegisterUsableItem('santas_sugar_stick', function(source)
+    TriggerClientEvent('GMD_Christmas:usedDeerItem', source)
+end)
+
+ESX.RegisterUsableItem('sugar_cone', function(source)
+
+end)
+
+ESX.RegisterUsableItem('santas_coffee', function(source)
+
+end)
+
+ESX.RegisterUsableItem('santas_wine', function(source)
+
+end)
+
+ESX.RegisterUsableItem('gingerbread', function(source)
+
+end)
