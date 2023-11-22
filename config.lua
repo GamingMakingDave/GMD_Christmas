123a0f9b9eab01e7c0936049bf5b94eafb417f84
GMD <87772503+GamingMakingDave@users.noreply.github.com>
2023-11-22 23:23:34 +0100

    update config.lua

--- config.lua
+++ config.lua
@@ -1,33 +1,34 @@
 Config = {}
 
 Config.Local = "de"
+Config.Debug = true -- set this false in Roleplay for settingup if you need true
 
-Config.UseXmasWeather = true
+Config.UseXmasWeather = true -- set false if you use other weather script
 
 -- CHRISTMAS MARKET OPTIONS
-Config.ChristmasMarket = vector3(-1662.180, -1082.309, 12.152)
-Config.ChristmasMarketRadius = 100.00
-Config.ChristmasMusicLink = "https://www.youtube.com/watch?v=lw-vF-ERE0w"
-Config.ChristmasMusicVolume = 0.1
+Config.ChristmasMarket = vector3(-1662.180, -1082.309, 12.152) -- Christmas-market coords use middle!!
+Config.ChristmasMarketRadius = 100.00 -- Play music on Christmas-market in radius!
+Config.ChristmasMusicLink = "https://www.youtube.com/watch?v=lw-vF-ERE0w" -- link to you xmas music from youtube, look for Streamer Copyright Free music!!!
+Config.ChristmasMusicVolume = 0.1 -- Music Volume
 
 -- SANTA CLAUS OPRIONS
-Config.SantaClausModel = "Santaclaus"
-Config.SantaClausPosition = vector3(-1620.303, -1050.869, 12.151)
-Config.SantaClausHeading = 149.264
-Config.SantaClausRandoms = 50 
+Config.SantaClausModel = "Santaclaus" -- Santa Ped Model
+Config.SantaClausPosition = vector3(-1620.303, -1050.869, 12.151) -- Position from Santa
+Config.SantaClausHeading = 149.264 -- Heading from Santa
 
 -- DEER MISSION
 Config.DeerZones = {
     {
-        DeerCount = 5,
-        DeerPedModel = "a_c_deer",
-        DeerZonesRadius = 50.0,
-        DeerFinishedItem = "christmas_stocking",
+        DeerCount = 5, -- Find Deer Count 
+        DeerPedModel = "a_c_deer", -- Deer Ped Model
+        DeerZonesRadius = 50.0, -- Deer wandering Radius
 
-        DeerRandomTicketCount = 5,
-        GoldTicker = "gold_ticket",
+        DeerFinishedItem = "christmas_stocking", -- DO NOT CHANGE IF NO IDEA
 
-        DeerFinishedSugarItems = 10,
+        DeerRandomTicketCount = 5, -- Max Gold-Ticket count is all time random from 1 to DeerRandomTicketCount(5)
+        GoldTicker = "gold_ticket", -- DO NOT CHANGE IF NO IDEA
+
+        DeerFinishedSugarItems = 10, -- Max Item count is all time random from 1 to DeerFinishedSugarItems(10)
         DeerFinishedItems = {
             "sugar_cone",
             "santas_coffee",
@@ -48,6 +49,7 @@ Config.DeerZones = {
         vector4(2953.103, 2789.059, 41.513, 100.00),
         vector4(1292.175, 2848.248, 49.363, 100.00),
         vector4(780.621, 2540.892, 74.860, 100.00)
+         -- here you can add more random Coords for Deer spawn
         }
     }
 }
@@ -55,10 +57,9 @@ Config.DeerZones = {
 -- GIFT JOB
 Config.GiftJob = {
     {
-        EnableChristmasDoorAnim = true,
-        GiftItemName = "christmas_giftbox",
-        GiftCount = 5,
-        GiftEntryRandom = 50,
+        GiftItemName = "christmas_giftbox", -- ussage BzZzi´s gift prop
+        GiftCount = 5, -- Max random Gift Count to finished Giftjob
+        GiftRandomTicket = 4, -- Max random Gold-Ticket for Giftjob
         CoordsGift = {
             vector4(-935.000, -939.290, 2.145, 122.77),
             vector4(-913.426, -989.951, 2.150, 208.816),
@@ -77,6 +78,7 @@ Config.GiftJob = {
             vector4(167.404, 473.754, 142.513, 268.082),
             vector4(119.668, 494.253, 147.342, 278.534),
             vector4(57.750, 449.765, 147.031, 142.563)
+            -- here you can add more random Coords for Giftjob to earn Tickets
         }
     }
 }
@@ -86,6 +88,22 @@ Config.GiftJob = {
 Config.Language = {
     ['de'] = {
         ['santa_helptext'] = 'Drücke ~INPUT_PICKUP~ um mir zu helfen.',
-        ['has_found_deer'] = 'Nutze nun das item was ich dir gegeben habe'
+        ['santa_distance_massage'] = 'Mein Rentier ist in der nähe halte deine Augen auf!',
+        ['has_found_deer'] = 'Nutze nun das item was ich dir gegeben habe',
+        ['santa_found_massage'] = 'Ho ho Ho HO, du hast eins meiner Rentiere gefunden dir fehlen noch %s meiner Rentiere!',
+
+        -- Scaleform
+        ['scaleform_big_text'] = 'MISSION PASSED',
+        ['scaleform_small_text'] = 'HO ho HO... Du hast all meine Rentiere gefunden, hier ein Dankeschön für deine Mühe!',
+
+        -- Giftjob
+        ['npc_hello'] = 'Hi?',
+        ['npc_thanks_for_gift'] = 'Ein Geschenk? Für mich Danke dir!',
+
+        -- Item used
+        ['used_sugar_cone'] = 'Du hast eine Zuckerstange gegessen.',
+        ['used_gingerbread'] = 'Du hast ein Lebkuchen gegessen.',
+        ['santas_coffee'] = 'Du hast Santas Kaffee getrunken, spürst du diese Energie?',
+        ['santas_wine'] = 'Du hast Santas Wein getrunken, mensch ist der Start , dass erklärt seine Rotenbacken! Hucks'
     }
 }
