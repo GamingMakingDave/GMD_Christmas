6336ae1af4b5a2715b40207d654ccf989c5fca23
GMD <87772503+GamingMakingDave@users.noreply.github.com>
2023-11-25 01:00:24 +0100

    delete useless shit

--- server/server.lua
+++ server/server.lua
@@ -43,47 +43,10 @@ AddEventHandler('GMD_Christmas:giveXmasSearchStocking', function()
         ['@hasFinishedDeerGame'] = 1,
         ['@identifier'] = xPlayer.getIdentifier()
     })
+
     xPlayer.addInventoryItem('christmas_stocking', 1)
 
 end)
-
--- ESX.RegisterUsableItem('christmas_stocking', function(source)
---     local xPlayer = ESX.GetPlayerFromId(source)
---     local CanCarryItems = true
---     local Items = {}
-
---     for i, item in pairs(Config.DeerZones[1].DeerFinishedItems) do
-        
---         local random = math.random(1, Config.DeerZones[1].DeerFinishedSugarItems)
-
---         if xPlayer.canCarryItem(item, random) then
---             table.insert(Items, {item, random})
---         else
---             CanCarryItems = false
---             break
---         end
---     end
-
---     if not CanCarryItems then
---         print("dein Inventar ist zu voll für die items lol")
---     else
---         for k, v in ipairs(Items) do
---             xPlayer.addInventoryItem(v.item, v.random)
---         end
-
---         xPlayer.addInventoryItem(Config.DeerZones[1].GoldTicker, Config.DeerZones[1].DeerRandomTicketCount)
---         xPlayer.removeInventoryItem('christmas_stocking', 1)
---     end
--- end)
-
--- ESX.RegisterServerCallback('GMD_Christmas:HasSearchItem', function(source, cb)
---     local xPlayer = ESX.GetPlayerFromId(source)
---     if xPlayer.getInventoryItem('christmas_stocking') ~= nil and xPlayer.getInventoryItem('christmas_stocking').count >= 1 then
---         cb(true)
---     else
---         cb(false)
---     end
--- end)
 
 ESX.RegisterServerCallback('GMD_Christmas:HasMissionFinished', function(source, cb)
     local xPlayer = ESX.GetPlayerFromId(source)
@@ -91,14 +54,6 @@ ESX.RegisterServerCallback('GMD_Christmas:HasMissionFinished', function(source, 
     MySQL.Async.fetchScalar('SELECT hasFinishedDeerGame FROM xmas WHERE identifier = @identifier', {
         ['@identifier'] = xPlayer.getIdentifier()
     }, function(hasFinishedDeerGame)
-        if hasFinishedDeerGame == nil then
-            MySQL.Async.execute('INSERT INTO xmas (identifier, hasFinishedDeerGame) VALUES (@identifier, 0)', {
-                ['@identifier'] = xPlayer.getIdentifier()
-            })
-            cb(false)
-            
-        else
-            print(hasFinishedDeerGame)
             if hasFinishedDeerGame then
                 cb(true)
             else
@@ -108,3 +63,23 @@ ESX.RegisterServerCallback('GMD_Christmas:HasMissionFinished', function(source, 
     end)
 end)
 
+
+
+-- GMD_Christmas:HasFoundDeers
+-- AddEventHandler('playerSpawned', function ()
+--     print('LOL')
+--   end)
+
+-- AddEventHandler('playerSpawned', function()
+--     print("Gespawnt")
+--     MySQL.Async.fetchScalar('SELECT hasFinishedDeerGame FROM xmas WHERE identifier = @identifier', {
+--         ['@identifier'] = xPlayer.getIdentifier()
+--     }, function(hasFinishedDeerGame)
+--         if hasFinishedDeerGame == nil or 0 then
+--             print("Nicht Fertig") 
+--         end
+--         if hasFinishedDeerGame then
+--            print("Fertig")
+--         end
+--     end)
+--  end)
